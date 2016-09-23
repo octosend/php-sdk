05b7650e322335d45cdbc038470308cec0b65c71
Lucas Filippi <lucas.filippi@dalenys.com>
2016-09-23 15:06:39 +0200

    APIError: improve standard message

--- src/OctoSend/APIError.php
+++ src/OctoSend/APIError.php
@@ -4,14 +4,14 @@ namespace OctoSend;
 
 class APIError extends \Exception
 {
-        function __construct($http_version, $http_status_code, $http_status, $http_headers, $body)
-	{
-		$this->http_version = $http_version;
-		$this->http_status_code = $http_status_code;
-		$this->http_status = $http_status;
-		$this->http_headers = $http_headers;
-		$this->body = $body;
-	}
-}
+    public function __construct($http_version, $http_status_code, $http_status, $http_headers, $body)
+    {
+        $this->http_version = $http_version;
+        $this->http_status_code = $http_status_code;
+        $this->http_status = $http_status;
+        $this->http_headers = $http_headers;
+        $this->body = $body;
 
-?>
+        parent::__construct("Octosend API return HTTP code " . $this->http_status_code .": \"".$this->http_status."\"", $this->http_status_code);
+    }
+}
